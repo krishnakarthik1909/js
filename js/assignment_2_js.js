4d151ff33b71cd43576d9c1a913608fe4bcd4abf
krishnakarthik1909 <64801216+krishnakarthik1909@users.noreply.github.com>
2020-05-04 23:04:43 +0530

    Add files via upload

---
+++ js/assignment_2_js.js
@@ -0,0 +1,10 @@
+function billingFunction(){
+		if (same.checked){
+document.getElementById("billingName").value=document.getElementById("shippingName").value;
+document.getElementById("billingZip").value=document.getElementById("shippingZip").value;
+    }
+	else{
+		document.getElementById("billingName").value="";
+		document.getElementById("billingZip").value="";
+	}
+}
